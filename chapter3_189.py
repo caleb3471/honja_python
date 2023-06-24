90cd7165eb19707601fe09ad825c1318e15a3b9f
caleb <caleb3471@gmail.com>
2023-06-24 19:09:11 +0900

    if를 활용한 약수 계산

---
+++ chapter3_189.py
@@ -0,0 +1,24 @@
+input_num = int(input("정수를 입력하세요."))
+
+if input_num%2 == 0:
+    print("{}는 2로 나누어 떨어지는 숫자입니다.".format(input_num))
+else:
+    print("{}는 2로 나누어 떨어지는 숫자가 아닙니다.".format(input_num))
+    
+
+if input_num%3 == 0:
+    print("{}는 3로 나누어 떨어지는 숫자입니다.".format(input_num))
+else:
+    print("{}는 3로 나누어 떨어지는 숫자가 아닙니다.".format(input_num))
+    
+
+if input_num%4 == 0:
+    print("{}는 4로 나누어 떨어지는 숫자입니다.".format(input_num))
+else:
+    print("{}는 4로 나누어 떨어지는 숫자가 아닙니다.".format(input_num))
+    
+
+if input_num%5 == 0:
+    print("{}는 5로 나누어 떨어지는 숫자입니다.".format(input_num))
+else:
+    print("{}는 5로 나누어 떨어지는 숫자가 아닙니다.".format(input_num))
