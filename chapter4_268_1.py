4e30345071642f9942227a5e7b5637f34be406ee
caleb <caleb3471@gmail.com>
2023-06-27 00:40:55 +0900

    리스트에 숫자 개수 구하기

---
+++ chapter4_268_1.py
@@ -0,0 +1,15 @@
+list_q = [1,2,3,4,1,2,3,1,4,1,2,3]
+
+list_1 = [i for i in list_q if i==1]
+list_2 = [i for i in list_q if i==2]
+list_3 = [i for i in list_q if i==3]
+list_4 = [i for i in list_q if i==4]
+
+dict_al = {
+    1 : len(list_1),
+    2 : len(list_2),
+    3 : len(list_3),
+    4 : len(list_4)
+}
+
+print(dict_al)
