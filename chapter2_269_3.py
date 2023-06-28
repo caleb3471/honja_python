1277e25c42cbed63325b6f0519a2b9ba08223469
caleb <caleb3471@gmail.com>
2023-06-28 23:22:07 +0900

    염기서열의 종류와 개수구하기

--- chapter2_269_3.py
+++ chapter2_269_3.py
@@ -1,19 +1,27 @@
 
-while(True):
-    dna_list = input("염기 서열을 입력해주세요. > ")
+
+dna_list = input("염기 서열을 입력해주세요. > ")
     
-    codonlist = []
+codonlist = []
+codon_dict = {}
     
     
-    for i in range(0, len(dna_list)//3):
-        print(len(dna_list))
-        for j in range(0,3):
-            codon_name=""
-            codon_name += dna_list[0]
-            dna_list = dna_list[1:]
-            print(codon_name)
-            
-    """codonlist.append(codon_name)        
-    print(codonlist[i])        
-    """
+for i in range(0, len(dna_list)//3):
+    codon_name=""
+    for j in range(0,3):
+        
+        codon_name += dna_list[0]
+        dna_list = dna_list[1:]
+   
+    codonlist.append(codon_name)        
+
+    if codon_name not in codon_dict:
+        codon_dict[codon_name] = 1
+    else:
+        codon_dict[codon_name] = codon_dict.get(codon_name) +1
+        
+
+
+print(codon_dict)
+
 
